89819dd73a849d692752728796aa34820f73b28e
amirali taghishah <amirsihezar@hotmail.com>
2023-02-28 21:23:09 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+     <p> Best Fitness App Ever</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+
+const Navbar = () => {
+  return (
+    <div className="Navbar-bg">This is going to be an awesome Navbar</div>
+  )
+}
+
+export default Navbar
