e19fb36ea0c51d2108619d0b3c056a1f5ee73687
amirali taghishah <amirsihezar@hotmail.com>
2023-03-01 08:44:13 +0330

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-     <p> Best Fitness App Ever</p>
+     <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> Best Fitness App Ever</p>
     </div>
   )
 }
