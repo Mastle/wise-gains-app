89762183c5c27f3fec21e73cfbdb043917453986
amirali taghishah <amirsihezar@hotmail.com>
2023-03-01 21:05:46 +0330

    project requirements fully set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import Navbar from './components/Navbar'
+import Button from './components/Button'
 import './App.css'
 
 const App = () => {
+  const handleClick = () => {
+    let sum = 0
+    ++sum
+    console.log(sum)
+  }
+
   return (
+
     <div className="App">
-      <Navbar />
-     <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> Best Fitness App Ever</p>
+      {/* <Navbar /> */}
+      <Button onClick={handleClick}/>
+     <p className="text-darkGrayishBlue px-3 py-2 text-4xl"> Best Fitness App Ever {'hello'}</p>
     </div>
   )
 }
 
 export default App
+
+/*
+
+TODO:
+ 1- Test a click event on a component
+ 2- Start building the Navbar following Brad's Tailwind and React tuts
+
+After production:
+-Make sure you run react tests after development is finished
+
+*/
