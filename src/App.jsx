cd9e0449a76aaaabf344b524c5d5c25f1ab63fd1
amirali taghishah <amirsihezar@hotmail.com>
2023-03-05 19:40:23 +0330

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import Navbar from './components/Navbar'
-import Button from './components/Button'
 import './App.css'
 
 const App = () => {
-  const handleClick = () => {
-    let sum = 0
-    ++sum
-    console.log(sum)
-  }
 
   return (
 
-    <div className="App">
-      {/* <Navbar /> */}
-      <Button onClick={handleClick}/>
-     <p className="text-darkGrayishBlue px-3 py-2 text-4xl"> Best Fitness App Ever {'hello'}</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
@@ -24,10 +16,8 @@ export default App
 /*
 
 TODO:
- 1- Test a click event on a component
- 2- Start building the Navbar following Brad's Tailwind and React tuts
-
-After production:
--Make sure you run react tests after development is finished
+  1- Follow Brad's tailwind tut website as a template
+  2- Add a "Testimonials" section
+  3- 
 
 */
